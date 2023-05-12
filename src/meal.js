3caf1d35d538e362b7c78429f20691e065dcaf53
Prissilla Escobar <prissilla988@gmail.com>
2023-05-11 22:08:55 -0600

    complete as many functions as I can

--- src/meal.js
+++ src/meal.js
@@ -11,14 +11,37 @@ function createMenuItem(name, price, type) {
   return menuItem
 }
 
+function addIngredients(food, ingredients) {
+  return [ingredients.push(food)]  
+  }
+
+
+function formatPrice(newPrice) {
+  return (`$${newPrice}`)
+
+}
+
+function decreasePrice(decreasedPrice, price) {
+  return (decreasedPrice*.9)
+}
+
+function createRecipe(recipe, ingredients, type) {
+  var recipe = {
+    recipe: menuItem.name,
+    ingredients: ingredients,
+    type: type
+
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
