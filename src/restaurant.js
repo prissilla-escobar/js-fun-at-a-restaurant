2142d8ad1b959f1f7a9cb028dd920fce6540dd92
Prissilla Escobar <prissilla988@gmail.com>
2023-05-14 15:47:36 -0600

    complete all functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,44 @@
 
 
+function createRestaurant(name) {
+return pizzaRestaurant = {
+  name: name,
+  menus: {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+  },
+}
+}
+
+function addMenuItem(pizzaRestaurant, foodType) {
+  if (pizzaRestaurant.menus[foodType.type].includes(foodType) != true) 
+  pizzaRestaurant.menus[foodType.type].push(foodType)
+}
+
+function removeMenuItem(pizzaRestaraunt, foodType, dayTime) {
+  var menu = pizzaRestaurant.menus[dayTime]
+  for (var i =0; i < menu.length; i++) {
+    if (menu[i].name === foodType)
+    menu.splice(i, 1)
+    return `No one is eating our ${foodType} - it has been removed from the ${dayTime} menu!`
+  }
+    return `Sorry, we don't sell ${foodType}, try adding a new recipe!`
+}
+
+function checkForFood(pizzaRestaurant, foodItem) {
+  var menu = pizzaRestaurant.menus[foodItem.type]
+  for (var i =0; i < menu.length; i++) {
+    if (menu[i].name === foodItem.name) {
+    return `Yes, we're serving ${foodItem.name} today!`
+    } 
+}
+}
+
+
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem,
+  checkForFood
 }
